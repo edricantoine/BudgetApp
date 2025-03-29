720f7d483d269d723fc54b87464611234e717644
Edric Antoine <edric.c.antoine@gmail.com>
2025-03-29 13:14:50 -0700

    Initial Commit

---
+++ Budget/BudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetApp.swift
+//  Budget
+//
+//  Created by Edric Antoine on 2025-03-29.
+//
+
+import SwiftUI
+
+@main
+struct BudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BudgetTests/BudgetTests.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetTests.swift
+//  BudgetTests
+//
+//  Created by Edric Antoine on 2025-03-29.
+//
+
+import Testing
+@testable import Budget
+
+struct BudgetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
